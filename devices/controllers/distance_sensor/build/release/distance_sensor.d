7c09a3bbf02921666cb3014f5dd6b905f3d47ba7
Andrej Bantulic <52709118+bantula@users.noreply.github.com>
2023-06-23 16:16:59 +0200

    Add files via upload

---
+++ devices/controllers/distance_sensor/build/release/distance_sensor.d
@@ -0,0 +1,6 @@
+build/release/distance_sensor.o: distance_sensor.c \
+ C:\Users\Windows\ User\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\Windows\ User\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\Windows\ User\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\Windows\ User\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\Windows\ User\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
